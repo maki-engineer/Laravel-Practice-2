96f33a08c43e9997d8bfb5e8506f4c7ea8109c1e
maki-engineer <ryohei-81@outlook.jp>
2022-08-23 10:51:55 +0900

    update: 投稿だけでなく、編集や削除もログインしているユーザーしか出来ないように設定

--- routes/web.php
+++ routes/web.php
@@ -22,17 +22,18 @@ Route::get("/sample/{id}", "App\Http\Controllers\Sample\IndexController@showId")
 
 Route::get("/tweet", "App\Http\Controllers\Tweet\IndexController")
 ->name("tweet.index");
-Route::post("/tweet/create", "App\Http\Controllers\Tweet\CreateController")
-->middleware("auth")
-->name("tweet.create");
-Route::get("/tweet/update/{tweetId}", "App\Http\Controllers\Tweet\Update\IndexController")
-->name("tweet.update.index")
-->where("tweetId", "[0-9]+");
-Route::put("/tweet/update/{tweetId}", "App\Http\Controllers\Tweet\Update\PutController")
-->name("tweet.update.put")
-->where("tweetId", "[0-9]+");
-Route::delete("/tweet/delete/{tweetId}", "App\Http\Controllers\Tweet\DeleteController")
-->name("tweet.delete");
+Route::middleware('auth')->group(function(){
+    Route::post("/tweet/create", "App\Http\Controllers\Tweet\CreateController")
+    ->name("tweet.create");
+    Route::get("/tweet/update/{tweetId}", "App\Http\Controllers\Tweet\Update\IndexController")
+    ->name("tweet.update.index")
+    ->where("tweetId", "[0-9]+");
+    Route::put("/tweet/update/{tweetId}", "App\Http\Controllers\Tweet\Update\PutController")
+    ->name("tweet.update.put")
+    ->where("tweetId", "[0-9]+");
+    Route::delete("/tweet/delete/{tweetId}", "App\Http\Controllers\Tweet\DeleteController")
+    ->name("tweet.delete");
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
