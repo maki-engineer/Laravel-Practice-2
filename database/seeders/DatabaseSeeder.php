49065aa2f82ee02bfec2af29015ffb0e13173a4e
maki-engineer <ryohei-81@outlook.jp>
2022-08-23 10:33:20 +0900

    update: マイグレーションとシーディングを初期化

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        $this->call(UsersSeeder::class);
         $this->call(TweetSeeder::class);
 
         // \App\Models\User::factory()->create([
