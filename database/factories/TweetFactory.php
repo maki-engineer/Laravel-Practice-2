2c30e1e438b3ff5ab3f8a0dd06b4bb93d8216ef4
maki-engineer <ryohei-81@outlook.jp>
2022-08-25 11:19:24 +0900

    update: 昨日は何件のツイートが投稿されたかをメールで送信するコマンドを作成

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tweet>
@@ -17,8 +18,9 @@ class TweetFactory extends Factory
     public function definition()
     {
         return [
-            "user_id" => 1,
-            "content" => $this->faker->realText(100)
+            "user_id"    => 1,
+            "content"    => $this->faker->realText(100),
+            'created_at' => Carbon::now()->yesterday()
         ];
     }
 }
