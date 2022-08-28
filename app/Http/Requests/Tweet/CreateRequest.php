1164b46c20f9b0f2a4ab9a22f984f7e1db912e2d
maki-engineer <ryohei-81@outlook.jp>
2022-08-28 11:15:50 +0900

    update: 画像と一緒にツイートできるように設定

--- app/Http/Requests/Tweet/CreateRequest.php
+++ app/Http/Requests/Tweet/CreateRequest.php
@@ -24,8 +24,15 @@ class CreateRequest extends FormRequest
     public function rules()
     {
         return [
-            "tweet" => "required|max:140"
+            "tweet"    => "required|max:140",
+            "images"   => "array|max:4",
+            "images.*" => "required|image|mimes:jpeg,png,jpg,gif|max:2048"
         ];
+    }
+
+    public function images(): array
+    {
+        return $this->file("images", []);
     }
 
     // Requestクラスのuser関数で今自分がログインしているユーザーが取得できる
